0ff3d31bac06ff7ad0542a6c6f14809c40e97d9e
GUANTAI001 <jpguantai@gmail.com>
2023-03-28 20:09:43 +0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :pays
   resources :books
   resources :destnations
-  resources :destinations
   resources :usrs
   resources :payments
   resources :bookings
@@ -10,6 +9,6 @@ Rails.application.routes.draw do
   # resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get"/", to: "destinations#index"
+  get"/", to: "destintions#index"
 
 end
