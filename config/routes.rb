2cf55ee535b4aa6f687a90f6bfe322bd19bbb31d
Rawllings Kiprop <rutojr01@gmail.com>
2023-03-30 14:56:02 +0300

    user

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
   post '/signup', to: "usrs#create"
   delete '/logout', to: "sessions#destroy"
 
-  get "/loggedin", to: "application#logged"
-
+  get "/loggedin", to: "usrs#loggedin"
+  
 
 
   # Books Routes
